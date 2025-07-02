37e46ae07a801dcda71067246a4bdd2fd6610189
theoUniv <149110726+theoUniv@users.noreply.github.com>
2025-07-02 10:16:56 +0200

    swagger comment

--- backend/swagger.js
+++ backend/swagger.js
@@ -4,7 +4,7 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'API Résumeur Juridique',
+            title: 'API Résumeur PDF',
             version: '1.0.0',
             description: 'API pour résumer des documents PDF ou du texte brut',
         },
